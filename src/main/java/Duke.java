3f0c70dc844da8a407bc1a7dfc47253c56d1df79
Jess Teo <e0309463@u.nus.edu.sg>
2019-08-22 18:57:37 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Duke {
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+        }
+
+        //...
+    }
     public static void main(String[] args) throws IOException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -13,21 +28,29 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        List list = new ArrayList();
+        List<Task> list = new ArrayList<Task>();
         boolean flag = true;
         while(flag) {
             String name = reader.readLine();
+            Task t = new Task(name);
             if(name.equals("bye")){
                 flag = false;
                 System.out.println("Bye! Hope to see you again soon!");
             }
             else if (name.equals("list")){
+                System.out.println("Here are the tasks in your list:");
                 for(int i=0; i<list.size(); i++){
-                    System.out.println(i+1 +". " + list.get(i).toString());
+                    System.out.println(i+1 +".[" +list.get(i).getStatusIcon() +"]"+ list.get(i).description );
                 }
             }
+            else if (name.startsWith("done")){
+                int numbercheck = Integer.parseInt(name.substring(5)) - 1;
+                list.get(numbercheck).isDone = true;
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println("[" + list.get(numbercheck).getStatusIcon() + "]" + list.get(numbercheck).description);
+            }
             else {
-                list.add(name);
+                list.add(t);
                 System.out.println("added: "+ name);
             }
         }
